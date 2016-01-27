73cdeabcf626023c209a3bd8de47a3b037968f84
HelloArnie <amiarnabbolchi@gmail.com>
2016-01-28 00:26:53 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,32 @@
  */
 #include <iostream>
 #include <armadillo>
+#include <string>
 
 using namespace std;
 using namespace arma;
 
+void ReadInput(string filename)
+{
+	cout << "Not implemented yet!\n";
+}
+
 int main()
 {
+	cout << "Starting program...\n";
+
+	ReadInput("InputFile.mat");
+
+	mat X = randu<mat>(5,5);
+
+	mat U;
+	vec s;
+	mat V;
+
+	svd(U,s,V,X);
+	cout << U << endl;
+	cout << s << endl;
+	cout << V << endl;
 	return 0;
 }
 
