42758c6bef7fec3f1bcb04c9e95fc51432195d05
HelloArnie <amiarnabbolchi@gmail.com>
2016-01-27 20:35:50 +0530

    Added sources and project files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 27-Jan-2016
+ *      Author: arnab
+ */
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main()
+{
+	return 0;
+}
+
+
+
